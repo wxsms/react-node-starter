fc8e0e61d733381bb4f70f8ca508c269960a38d3
wxsm <wxsms@foxmail.com>
2018-10-09 16:42:52 +0800

    refactor

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
 import React, {PureComponent} from 'react';
-import {Route, Link, withRouter} from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import axios from 'axios';
-import {renderIf} from './utils/commonUtils';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
-import {Layout, Menu, Icon, Spin} from 'antd';
+import {Layout, Spin} from 'antd';
 import * as actions from './redux/actions';
+import PageHeader from './components/PageHeader';
 
-const SubMenu = Menu.SubMenu;
-const {Header, Content, Footer} = Layout;
+const {Content, Footer} = Layout;
 
 class App extends PureComponent {
-  constructor (props) {
-    super(props);
-    this.state = {
-      selectedKeys: []
-    };
-  }
-
-  correctSelectedMenu = () => {
-    const path = this.props.location.pathname;
-    if (path === '/') {
-      this.setState({selectedKeys: ['/']});
-    } else {
-      try {
-        this.setState({selectedKeys: ['/' + path.split('/')[1]]});
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  };
-
   componentDidMount = () => {
-    this.correctSelectedMenu();
     this.props.actions.fetchUser();
-  };
-
-  componentDidUpdate = (prevProps, prevState, snapshot) => {
-    const prePath = prevProps.location.pathname;
-    const path = this.props.location.pathname;
-    if (prePath !== path) {
-      this.correctSelectedMenu();
-    }
-  };
-
-  onLogout = () => {
-    axios.post('/api/auth/logout')
-      .then(res => {
-        window.location.href = '/';
-      });
   };
 
   render () {
     const loading = this.props.user.get('loading');
-    const user = this.props.user.get('user');
-    const username = this.props.user.getIn(['user', 'username'], '');
 
     if (loading) {
       return (
@@ -69,39 +29,7 @@ class App extends PureComponent {
 
     return (
       <Layout className="layout">
-        <Header>
-          <div className="logo"/>
-          <Menu theme="dark" mode="horizontal" selectedKeys={this.state.selectedKeys}
-                style={{lineHeight: '64px'}}>
-            <Menu.Item key="/">
-              <Link to={'/'}>Home</Link>
-            </Menu.Item>
-            {
-              renderIf(user)(
-                <SubMenu
-                  style={{float: 'right'}}
-                  title={(
-                    <span>
-                      <span>Welcome, {username}&nbsp;</span>
-                      <Icon type="caret-down" theme="outlined"/>
-                    </span>
-                  )}
-                >
-                  <Menu.Item key="/logout">
-                    <a role={'button'} onClick={this.onLogout}>Logout</a>
-                  </Menu.Item>
-                </SubMenu>
-              )
-            }
-            {
-              renderIf(!user)(
-                <Menu.Item key="/login" style={{float: 'right'}}>
-                  <Link to={'/login'}>Login</Link>
-                </Menu.Item>
-              )
-            }
-          </Menu>
-        </Header>
+        <PageHeader/>
         <Content style={{padding: '0 50px'}}>
           <div style={{background: '#fff', padding: 24, minHeight: 280, marginTop: 50}}>
             <Route exact path="/" component={Home}/>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import App from './App';
-import './App.css';
+import './index.css';
 
 ReactDOM.render((
   <Provider store={store}>
