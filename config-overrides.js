9dad0b1b33baa899fc8d625bb2f1bcaf3189683a
wxsm <wxsms@foxmail.com>
2020-07-12 22:00:42 +0800

    fix upgrade

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,11 @@
-const {injectBabelPlugin} = require('react-app-rewired');
+const {
+  override,
+  fixBabelImports
+} = require('customize-cra')
 
-module.exports = function override (config, env) {
-  config = injectBabelPlugin(
-    ['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}],
-    config,
-  );
-  return config;
-};
+module.exports = override(
+  fixBabelImports('babel-plugin-import', {
+    libraryName: 'antd',
+    style: 'css'
+  })
+)
