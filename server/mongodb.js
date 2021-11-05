fa1bdaff018f9e330ba8d1b9c8feb65b1e4f7963
guokairui <wxsms@foxmail.com>
2021-11-05 19:19:51 +0800

    fix: start error

--- server/mongodb.js
+++ server/mongodb.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 
 const url = process.env.DATABASE_URL || 'mongodb://localhost:27017'
 
-mongoose.set('useCreateIndex', true)
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
